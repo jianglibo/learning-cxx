3480b860f1e8198645d83cb36bbd25f3e181d7f7
jianglibo <jianglibo@gmail.com>
2024-08-20 21:23:34 +0800

    minor change.

--- gtest/reference_test.cpp
+++ gtest/reference_test.cpp
@@ -9,8 +9,8 @@ static void reference_to_array(int (&arr)[2])
 
 static void reference_to_array1(int *arr[2])
 {
-	*arr[0] = 2;
-	*arr[1] = 2;
+	(*arr)[0] = 2;
+	(*arr)[1] = 2;
 }
 
 static void reference_to_array2(int (*&arr)[2])
@@ -18,6 +18,48 @@ static void reference_to_array2(int (*&arr)[2])
 	*arr[0] = 2;
 	*arr[1] = 2;
 }
+
+static void reference_to_array3(int (**&arr)[2])
+{
+	*arr[0][0] = 2;
+	*arr[0][1] = 2;
+	(**arr)[0] = 2;
+	(**arr)[1] = 2;
+}
+
+static void reference_to_array4(int (&arr)[2][2])
+{
+	arr[0][0] = 2;
+	arr[0][1] = 2;
+	arr[1][0] = 2;
+	arr[1][1] = 2;
+}
+
+static void reference_to_array5(int (*arr)[2][2])
+{
+	(*arr)[0][0] = 2;
+	(*arr)[0][1] = 2;
+	(*arr)[1][0] = 2;
+	(*arr)[1][1] = 2;
+}
+
+static void reference_to_array6(int (**arr)[2][2])
+{
+	(**arr)[0][0] = 2;
+	(**arr)[0][1] = 2;
+	(**arr)[1][0] = 2;
+	(**arr)[1][1] = 2;
+}
+
+// array of pointers
+static void reference_to_array7(int (*arr[2])[2])
+{
+	(*arr[0])[0] = 2;
+	(*arr[0])[1] = 2;
+	(*arr[1])[0] = 2;
+	(*arr[1])[1] = 2;
+}
+
 TEST(ReferenceTest, ARRAY)
 {
 	int p[2] = {1, 1};
@@ -31,8 +73,15 @@ TEST(ReferenceTest, ARRAY)
 	delete p1[1];
 
 	int p2[2] = {1, 1};
-	int (*p2_ref)[2] = &p2;
+	int(*p2_ref)[2] = &p2;
 
 	reference_to_array2(p2_ref);
 	ASSERT_EQ(p2[0], 2) << "content of p2 should change.";
+
+	int p3[2] = {1, 1};
+	int(*p3_ref)[2] = &p3;
+	int(**p3_ref_ref)[2] = &p3_ref;
+
+	reference_to_array3(p3_ref_ref);
+	ASSERT_EQ(p3[0], 2) << "content of p3 should change.";
 }
