8239d9193c0855171b0877dfd242501a10921a4a
jianglibo <jianglibo@gmail.com>
2024-10-07 07:46:57 +0800

    minor change.

--- gtest/string_all_test.cpp
+++ gtest/string_all_test.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <memory>
+#include <iostream>
+#include <cstring> // For strcpy
 
 TEST(StringAllTest, const_char)
 {
@@ -8,9 +11,20 @@ TEST(StringAllTest, const_char)
 
 	std::string s = "";
 	ASSERT_EQ(s.size(), 0);
-	ASSERT_NE(s[1], '\0');
+	// ASSERT_NE(s[1], '\0');
 
 	char const *p1 = s.c_str();
+	ASSERT_EQ(strlen(p1), 0);
 	ASSERT_EQ(p1[0], '\0');
 
+	// ISO C++ forbids converting a string constant to ‘char*
+	// char *p2 = "abc";
+	// ASSERT_EQ(p2[3], '\0');
+	// std::string s1{p2};
+	// ASSERT_EQ(s1.size(), 3);
+
+	// make_shared<char[]> is not allowed, make shared is for single object.
+	std::unique_ptr<char[]> p3(new char[4]);
+	std::strcpy(p3.get(), "abc");
+	ASSERT_STREQ(p3.get(), "abc");
 }
